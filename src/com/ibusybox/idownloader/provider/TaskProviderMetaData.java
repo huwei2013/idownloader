68dc95f285280bca5d9abc10dab9da78a7338d57
huwei2013 <huwei2013@gmail.com>
2013-12-13 17:09:54 +0800

    Signed-off-by: huwei2013 <huwei2013@gmail.com>

---
+++ src/com/ibusybox/idownloader/provider/TaskProviderMetaData.java
@@ -0,0 +1,8 @@
+package com.ibusybox.idownloader.provider;
+
+public class TaskProviderMetaData {
+
+	public static final String AUTHORITY = "com.ibusybox.idownloader.provider.TaskProvider";
+	
+	
+}
